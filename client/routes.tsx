6516a8dc11d4d2fa77db11596292aaf425064c7d
Haruka Ogino <harukariq@gmail.com>
2024-04-29 10:49:08 +1200

    comment/uncomment things

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,13 +1,13 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App'
 import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
+// import About from './components/About'
+// import Projects from './components/Projects'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="projects" element={<Projects />} />
+    {/* <Route path="about" element={<About />} />
+    <Route path="projects" element={<Projects />} /> */}
   </Route>
 )
